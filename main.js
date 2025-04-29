a7a53bbacf0efcee144e5de3dc838ff2634bfe12
Gabriel Kassarp <gkassarp@gmail.com>
2025-04-29 11:04:14 +0200

    added title

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { Application } from "pixi.js";
+import { Application , Graphics, Text} from "pixi.js";
 
 
 (async()=> {
@@ -7,10 +7,28 @@ import { Application } from "pixi.js";
 
     await app.init({
         resizeTo:window,
-        antialias:true,   
+       
     });
 
+    const TextBox = new Graphics()
+    .rect(675, 260, 50, 150)
+    .fill({0xffea00 : 0.5})
+    .stroke({color: 0x00ff00, width: 2});
+
+    const Title = new Text({
+        text: "Visual Novel",
+        style: {
+            fontSize: 36,
+            fill: 0xffffff,
+            align: "center",
+        },
+    });
+    
+
     app.canvas.style.position = "absolute";
+    app.stage.addChild(Title);
+    app.stage.addChild(TextBox);
+    
 
     document.body.appendChild(app.canvas);
 
