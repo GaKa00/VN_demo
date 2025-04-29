16c7a3a5e7465453ae6f517e5041188628eee569
Gabriel Kassarp <gkassarp@gmail.com>
2025-04-29 10:16:19 +0200

    installed base libraries

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log( "Hello, World!" );
+import { Application } from "pixi.js";
+
+
+(async()=> {
+
+    const app = new Application({
+        width: window.innerWidth,
+        height: window.innerHeight,    
+    })
+
+    await app.init();
+
+    document.body.appendChild(app.canvas);
+
+})();
